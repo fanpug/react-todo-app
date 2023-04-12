61fef7c30c9a89918f3b020f085802844ccdcb57
Humberto Navarro <a320669@uach.mx>
2023-04-12 15:34:25 -0600

    Edit and delete buttons hide when task is completed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,8 +19,8 @@ export default function TodoItem({todo, toggleTodo, editTodo, deleteTodo}) {
     <li className='mb-1'>
       <input type='checkbox' className='w-6 h-6 mr-1 translate-y-0.5' checked={completed} onChange={handleTodoClick} />
       <span className={completed ? 'line-through' : '' }>{task}</span>
-      <button className='ml-2 mr-1 motion-safe:hover:scale-125 motion-safe:hover:-translate-y-0.5 motion-safe:transition' onClick={handleEdit}>✏️</button>
-      <button className='motion-safe:hover:scale-125 motion-safe:hover:-translate-y-0.5 motion-safe:transition' onClick={handleDelete}>❌</button>
+      <button className={completed ? 'hidden' : 'ml-2 mr-1 motion-safe:hover:scale-125 motion-safe:hover:-translate-y-0.5 motion-safe:transition'} onClick={handleEdit}>✏️</button>
+      <button className={completed ? 'hidden' : 'motion-safe:hover:scale-125 motion-safe:hover:-translate-y-0.5 motion-safe:transition'} onClick={handleDelete}>❌</button>
     </li>
   )
 }
