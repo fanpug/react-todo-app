0a5a90622ae2028390fca48005537775364f4ece
Humberto Navarro <a320669@uach.mx>
2023-04-16 14:18:45 -0600

    Rework firebase sign in and logout code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     GoogleAuthProvider,
     signInWithPopup,
@@ -12,19 +12,46 @@ import { BsPersonCircle, BsGoogle } from "react-icons/bs"
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
 
-const buttonClasses = 'bg-slate-200 text-gray-800 hover:bg-slate-300 active:bg-slate-400 font-bold uppercase text-sm px-5 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150';
+const buttonClasses = 'flex items-center justify-center w-60 bg-slate-200 text-gray-800 hover:bg-slate-300 active:bg-slate-400 font-bold uppercase text-sm px-5 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150';
+
+/* 
+    States
+    0 - Initialized
+    1 - Loading
+    2 - Logged in
+    3 - No one is logged in
+*/
 
 function App() {
     const [currentUser, setCurrentUser] = useState(null);
+    const [state, setCurrentState] = useState(0);
+    let loginStatus = <div>Loading...</div>;
+
+    useEffect(() => {
+        setCurrentState(1);
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setCurrentState(2);
+                if (user.isAnonymous) {
+                    user.displayName = "Anonymous";
+                    user.photoURL = "https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg";
+                }
+                console.log(user);
+                setCurrentUser(user);
+            } else {
+                setCurrentState(3);
+                console.log("No users logged in...");
+            }
+        })
+    }, []);
 
     const handleGoogleAuth = () => {
         const googleProvider = new GoogleAuthProvider();
         signInWithPopup(auth, googleProvider)
             .then((result) => {
                 // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                setCurrentUser(result.user);
+                //const credential = GoogleAuthProvider.credentialFromResult(result);
+                //const token = credential.accessToken;
                 //console.log("Signed in with google", result.user.displayName);
             }).catch((error) => {
                 console.error(error.code);
@@ -35,7 +62,6 @@ function App() {
     const handleAnonAuth = () => {
         signInAnonymously(auth)
         .then(() => {
-            setCurrentUser("Anonymous");
             //console.log("Signed in anonymously");
         })
         .catch((error) => {
@@ -49,26 +75,36 @@ function App() {
             setCurrentUser(null);
             //console.log("User signed out")
         }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
+            console.error(error.code);
+            alert(error.message);
         });
+    }
+
+    
+    if(state === 1) {
+        loginStatus = <div>Loading...</div>;
+    }
+
+    if(state === 3) {
+        loginStatus = <>
+                        <button className={`${buttonClasses} mt-10`} onClick={handleGoogleAuth}><BsGoogle className="inline mr-4" />Sign in with Google</button>
+                        <button className={`${buttonClasses} mt-4`} onClick={handleAnonAuth}><BsPersonCircle className="inline mr-4" />Sign in Anonymously</button>
+                    </>;
     }
 
     return (
         <>
-            <Navbar currentUser={currentUser} handleLogout={handleLogout} />
+            <Navbar currentUser={currentUser} state={state} handleLogout={handleLogout} />
             <main className="flex flex-col items-center justify-center">
-                {!currentUser ?
+                {state !== 2 ?
                     <>
-                        <button className={`${buttonClasses} w-60 mt-10 flex items-center justify-center`} onClick={handleGoogleAuth}><BsGoogle className="inline mr-4" />Sign in with Google</button>
-                        <button className={`${buttonClasses} w-60 mt-4 flex items-center justify-center`} onClick={handleAnonAuth}><BsPersonCircle className="inline mr-4" />Sign in Anonymously</button>
+                        {loginStatus}
                     </> 
                 :
                     <>                        
                         <Todo />
                     </>
                 }
-                
             </main>
         </>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { BiLogOut } from "react-icons/bi"
 
 const buttonClasses = 'bg-slate-200 text-gray-800 hover:bg-slate-300 active:bg-slate-400 font-bold uppercase text-sm px-5 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150';
 
-export default function Navbar({currentUser, handleLogout}) {
+export default function Navbar({currentUser, state, handleLogout}) {
   return (
     <header className='sticky left-0 bottom-0 shadow-md z-40'>
         <nav className='h-[72px] min-h-full w-full flex items-center justify-between bg-red-300'>
                 <li className='list-none pl-14'><AboutModal /></li>
-                {currentUser ?
+                {state === 2 ?
                   <>
                     <li className='list-none pr-14'><button className={`${buttonClasses} flex items-center justify-center`} onClick={handleLogout}><BiLogOut className="text-lg inline mr-2" />Log Out</button></li>
                   </>
