9cb035ee2fba3dfcdbcc477f989e807d6cc03b0e
Humberto Navarro <a320669@uach.mx>
2023-04-15 21:59:45 -0600

    Add firebase authentication, anonymous and google sign in and logout buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    signInAnonymously,
+    onAuthStateChanged,
+    signOut,
+} from "firebase/auth";
+import { auth } from "./firebase/firebase";
+
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
 
-function App() {
-  
+const buttonClasses = 'w-52 bg-slate-200 text-gray-800 hover:bg-slate-300 active:bg-slate-400 font-bold uppercase text-sm px-5 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150';
 
-  return (
-    <>
-      <Navbar />
-      <main className="flex flex-col items-center justify-center">
-        <Todo />
-      </main>
-    </>
-  );
+function App() {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const handleGoogleAuth = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                setCurrentUser(result.user);
+                //console.log("Signed in with google", result.user.displayName);
+            }).catch((error) => {
+                console.error(error.code);
+                alert(error.message);
+            });
+    }
+    
+    const handleAnonAuth = () => {
+        signInAnonymously(auth)
+        .then(() => {
+            setCurrentUser("Anonymous");
+            //console.log("Signed in anonymously");
+        })
+        .catch((error) => {
+            console.error(error.code);
+            alert(error.message);
+        });
+    }
+
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            //console.log("User signed out")
+            setCurrentUser(null);
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    }
+
+    return (
+        <>
+            <Navbar />
+            <main className="flex flex-col items-center justify-center">
+                {!currentUser ?
+                    <>
+                        <button className={`${buttonClasses}`} onClick={handleGoogleAuth}>Sign in with Google</button>
+                        <button className={`${buttonClasses}`} onClick={handleAnonAuth}>Sign in Anonymously</button>
+                    </> 
+                :
+                    <>
+                        <button className={`${buttonClasses}`} onClick={handleLogout}>Log Out</button>
+                        <span className="capitalize">{currentUser.displayName || currentUser}</span>
+                        <Todo />
+                    </>
+                }
+                
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -22,8 +22,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
-const db = getFirestore(app);
+export const db = getFirestore(app);
